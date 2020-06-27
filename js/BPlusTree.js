2fcd85939737b3772b4347d80297cd60c9986c21
TreverHibbs <treverhibbs@gmail.com>
2020-06-26 18:20:06 -0700

    Add javascript code to create BPlusTree node

--- js/BPlusTree.js
+++ js/BPlusTree.js
@@ -9,3 +9,8 @@ ctx.lineWidth = 10;
 ctx.moveTo(450, 300);
 ctx.lineTo(600, 200);
 ctx.stroke();
+
+myBTreeNode = new AnimatedBTreeNode(0, 10, 10, 1, "white", "red");
+myBTreeNode.x = 400;
+myBTreeNode.y = 300;
+myBTreeNode.draw(ctx);
