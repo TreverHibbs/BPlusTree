ce24191d921ea89c958a313f946549c23d0cd43f
TreverHibbs <treverhibbs@gmail.com>
2020-07-06 15:47:26 -0700

    Add in browser javascript testing with mocha/chai

--- js/view/cmdConverter.js
+++ js/view/cmdConverter.js
@@ -49,3 +49,4 @@ function createCommand() {
   }
   return(command);
 }
+
--- test/test.js
+++ test/test.js
@@ -1,9 +1,30 @@
-var assert = require('assert');
-describe('Array', function () {
-      describe('#indexOf()', function () {
-              it('should return -1 when the value is not present', function () {
-                        assert.equal([1, 2, 3].indexOf(4), -1);
-                      });
-            });
+var assert = chai.assert;
+
+var string1 = "hello";
+var string2 = "world";
+
+describe('String', function() {
+  it('should start empty', function() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    
+    
+    objectManager = new ObjectManager();
+    animationManager = new AnimationManager(objectManager);
+   
+    command = createCommand("CreateBTreeNode",idIndex , WIDTH_PER_ELEM, NODE_HEIGHT, 1, STARTING_X, STARTING_Y, BACKGROUND_COLOR,  FOREGROUND_COLOR);
+    commands = [];
+    commands[0] = command;
+    command = createCommand("SetText", idIndex, 1, 0);
+    commands[1] = command;
+    console.log(command);
+    
+    animationManager.StartNewAnimation(commands);
+    var string1 = "hello";
+    var string2 = "world";
+    
+    assert.equal(createCommand(string1, string2), "hello<;>world",
+                 "cmdConverter returns new animation command");
+  });
 });
 
