685e1659b5e2cab86c009783ea6dca820281be62
TreverHibbs <treverhibbs@gmail.com>
2020-07-09 11:05:35 -0700

    Add render.js module

---
+++ js/view/render.js
@@ -0,0 +1,33 @@
+/**
+ * @desc this file is for parsing render
+ *       commands and executing the right animations
+ * @author Trever Hibbs treverhibbs@gmail.com
+*/
+
+
+/*
+ * CONTENTS
+ *
+ * RENDER-FUNCTIONS
+ *
+ */
+
+
+
+
+
+/*------------------------------------*\
+  #RENDER-FUNCTIONS
+\*------------------------------------*/
+
+/**
+ *  @desc creates a list of commands for the creation a BTree root.
+ *  @param Array $commands - an Array of animation libaray commands.
+ *         int $value - the value of the root node,
+ *  @return Array - the updated list of generated commands
+ */ 
+function renderInsert(command) { 
+
+  return(true);
+}
+
